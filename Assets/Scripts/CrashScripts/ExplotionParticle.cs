5aa0518ec655e77fc2381762da1f2fcdaa6df4f7
keicoon <keicoon@naver.com>
2016-01-12 22:49:00 +0900

    1. Add ExplotionScript! # need cube or sphere GameObject. # add script(explotion.cs)
    2. Modify TitleScene
    # scene
    # image
    # script
    
    3. Delete (don't need) files
    # skybox
    # materials

---
+++ Assets/Scripts/CrashScripts/ExplotionParticle.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplotionParticle : MonoBehaviour
+{
+    public enum explotionName { cube = 0, sphere = 1 }
+    // particle_number
+    public int particle_num = 8;
+    public float exlpotion_speed = 100f;
+    public explotionName particle_name;
+
+    // force(intType)
+    public int trigger_force = 9;
+    // 
+    public float trigger_time = 7f;
+
+    private new Transform transform;
+    void Awake()
+    {
+        transform = GetComponent<Transform>();
+        prepareObject();
+    }
+    void prepareObject()
+    {
+        // scale 0.5
+        // position random
+        // number(parameter)
+        GameObject[] Child_GameObjects = new GameObject[particle_num];
+
+        Vector3 newScale = transform.localScale * 0.5f;
+        Bounds bounds = transform.GetComponent<Renderer>().bounds;
+        Vector3 min = bounds.min, max = bounds.max;
+
+        for (int index = 0; index < particle_num; index++)
+        {
+            Vector3 newPosition = new Vector3(
+           Random.Range(min.x, max.x),
+           Random.Range(min.y, max.y),
+           Random.Range(min.z, max.z));
+
+            GameObject Child_GameObject = null;
+
+            if(particle_name == explotionName.cube)
+                Child_GameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            else if(particle_name == explotionName.sphere)
+                Child_GameObject = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+            Child_GameObject.transform.parent = transform;
+            Child_GameObject.transform.position = newPosition;
+            Child_GameObject.transform.localScale = newScale;
+
+            Child_GameObject.AddComponent<Rigidbody>();
+            Child_GameObject.GetComponent<Rigidbody>().useGravity = false;
+            Child_GameObject.GetComponent<Collider>().enabled = false;
+            Child_GameObject.SetActive(false);
+
+            Child_GameObjects[index] = Child_GameObject;
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        //enter the player
+        if (other.tag.CompareTo("Player") == 0)
+        {
+            //get player_force(particle_num)
+            int force_power = 10;//= other.GetComponent<scriptName>().particle_num;
+            if (force_power >= trigger_force)
+            {
+                OnExplotion(other.transform.position);//onExplotion
+            }
+            else
+            {
+                ;//notExplotion
+            }
+            transform.GetComponent<BoxCollider>().isTrigger = false;
+        }
+    }
+    void OnExplotion(Vector3 src_position)
+    {
+        int child_num = transform.childCount;
+        for (int index = 0; index < child_num; index++)
+        {
+            Transform child_transform = transform.GetChild(index);
+            GameObject child_gameobject = child_transform.gameObject;
+            child_gameobject.SetActive(true);
+            child_gameobject.GetComponent<Rigidbody>().AddForce(
+                (child_gameobject.transform.position - src_position) * exlpotion_speed,
+                ForceMode.Impulse);
+            StartCoroutine(disapear_child(child_gameobject, trigger_time));
+        }
+        transform.GetComponent<MeshRenderer>().enabled = false;
+    }
+     IEnumerator disapear_child(GameObject child, float limit_time)
+    {
+        float currentTime = 0f;
+        Vector3 rotateAxis = new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        rotateAxis.Normalize();
+        while (limit_time > currentTime)
+        {
+            child.transform.Rotate(rotateAxis * 360f * Time.deltaTime);
+            currentTime += Time.deltaTime;
+            yield return null;
+        }
+        child.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/MainScene/SceneManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour {
+
+    public GameObject Canavas;
+    public Image Character_img;
+    public Image BG_img;
+    
+    private Image _Button_img;
+    
+    public Sprite[] BG_imgs;
+    public float animation_time = 3f;
+
+    private float _current_time = 0f;
+    private int _imgAnimationIndex = 0;
+   
+    private float _animation_fps;
+
+    private Rect _canvas_bound;
+    private Transform _character_transform;
+    private Vector3 _range;
+    private float _button_color_speed = -1.5f;
+    void Awake() 
+    {
+        _Button_img = GameObject.FindGameObjectWithTag("Button").GetComponent<Image>();
+        _animation_fps = animation_time / BG_imgs.Length;
+        _canvas_bound = Canavas.GetComponent<Canvas>().pixelRect;
+        _character_transform = Character_img.GetComponent<Transform>();
+        _range = new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), 0f);
+        _range.Normalize();
+    }
+    void Update()
+    {
+        /// BG Animation
+        _current_time += Time.deltaTime;
+        // Change Image(=animation)
+        if (_current_time > _animation_fps)
+        {
+            ++_imgAnimationIndex;
+
+            if (_imgAnimationIndex >= BG_imgs.Length)
+                _imgAnimationIndex = 0;
+
+            BG_img.sprite = BG_imgs[_imgAnimationIndex];
+            _current_time = 0f;
+        }
+        /// END
+        /// Button Alpha Change
+        if(_Button_img.color.a > 1f || _Button_img.color.a < 0f)
+            _button_color_speed *= -1;
+        Color c = _Button_img.color;
+        c.a += _button_color_speed * Time.deltaTime;
+        _Button_img.color = c;
+        /// END
+        /// Chracter Move
+        if (_canvas_bound.xMin >= _character_transform.position.x
+                || _canvas_bound.xMax <= _character_transform.position.x)
+            _range.x *= -1f;
+        if (_canvas_bound.yMin >= _character_transform.position.y
+            || _canvas_bound.yMax <= _character_transform.position.y)
+            _range.y *= -1f;
+        // speed
+        float character_speed = 20f;
+        _character_transform.Translate(_range * character_speed * Time.deltaTime);
+        /// END
+    }
+    public void Button_Touch_Event() 
+    {
+        // change Scene
+        Debug.Log("Change Scene");
+        //Application.loadedLevel("game");
+    }
+}
